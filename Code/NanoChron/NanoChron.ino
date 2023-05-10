672a142a4a99a04a17a48f6b23e65040db19cb72
MoonriseSunset <aliuesch@gmail.com>
2023-05-09 17:44:23 -0700

    added the frame for the code

---
+++ Code/NanoChron/NanoChron.ino
@@ -0,0 +1,19 @@
+/*
+NanoChron
+
+Written by Adam E. (Moonrise_Sunset)
+
+Big thanks to Adafruit for making it so easy to use all these sensors :)
+
+This code is protected under GNU GPLv3
+*/
+
+void setup() {
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
